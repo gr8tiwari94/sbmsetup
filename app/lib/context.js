6899556f30226fff0e819ac4b3003f6a4100717b
gr8tiwari94 <118853188+gr8tiwari94@users.noreply.github.com>
2026-01-08 10:14:35 +0530

    Setup markets support using subfolders

--- app/lib/context.js
+++ app/lib/context.js
@@ -1,6 +1,7 @@
-import { createHydrogenContext } from "@shopify/hydrogen";
-import { AppSession } from "~/lib/session";
-import { CART_QUERY_FRAGMENT } from "~/lib/fragments";
+import {createHydrogenContext} from '@shopify/hydrogen';
+import {AppSession} from '~/lib/session';
+import {CART_QUERY_FRAGMENT} from '~/lib/fragments';
+import {getLocaleFromRequest} from '~/lib/i18n';
 
 // Define the additional context object
 const additionalContext = {
@@ -21,18 +22,18 @@ const additionalContext = {
 export async function createHydrogenRouterContext(
   request,
   env,
-  executionContext
+  executionContext,
 ) {
   /**
    * Open a cache instance in the worker and a custom session instance.
    */
   if (!env?.SESSION_SECRET) {
-    throw new Error("SESSION_SECRET environment variable is not set");
+    throw new Error('SESSION_SECRET environment variable is not set');
   }
 
   const waitUntil = executionContext.waitUntil.bind(executionContext);
   const [cache, session] = await Promise.all([
-    caches.open("hydrogen"),
+    caches.open('hydrogen'),
     AppSession.init(request, [env.SESSION_SECRET]),
   ]);
 
@@ -44,12 +45,12 @@ export async function createHydrogenRouterContext(
       waitUntil,
       session,
       // Or detect from URL path based on locale subpath, cookies, or any other strategy
-      i18n: { language: "EN", country: "US" },
+      i18n: getLocaleFromRequest(request),
       cart: {
         queryFragment: CART_QUERY_FRAGMENT,
       },
     },
-    additionalContext
+    additionalContext,
   );
 
   return hydrogenContext;
